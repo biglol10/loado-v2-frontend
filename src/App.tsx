8c19357332edf514f35b453b51da3efa00649a0b
81670 <jiwook@81670.local>
2023-01-26 18:19:19 +0900

    린트, 프리티어 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
 import './App.css';
 
 const App = () => {
-	return (
-		<div className='App'>
-
-		</div>
-	);
-
+  var a = 'sadf';
+  var b = 'qwer';
+  return <div className="App"></div>;
 };
 
 export default App;
