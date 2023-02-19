f7c1cd428de5c2dc63c6c5f44f7adc50537e559a
davidkim9204 <davidkim9204@naver.com>
2023-02-19 22:56:44 +0900

    feat: add table component without style

---
+++ src/components/atoms/table/Table.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-key */
+import { Column, useTable } from 'react-table';
+import { ITableData } from './Types';
+
+const Table = ({ columns, data }: { columns: Column[]; data: ITableData[] }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export { Table };
