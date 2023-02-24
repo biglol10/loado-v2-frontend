7373e5db1d4206472e8a3111d314a5d86682163a
biglol <biglol@nate.com>
2023-02-24 17:03:55 +0900

    changed loacommonutil

--- src/services/LoaCommonUtils.ts
+++ src/services/LoaCommonUtils.ts
@@ -1,17 +1,17 @@
 const returnFullSoomValues = (refineNumber: number) => {
-  if (refineNumber >= 12 && refineNumber <= 17) {
+  if (refineNumber >= 12 && refineNumber <= 13) {
     return {
       태양의은총: 24,
       태양의축복: 12,
       태양의가호: 4,
     };
-  } else if (refineNumber >= 18 && refineNumber <= 21) {
+  } else if (refineNumber >= 14 && refineNumber <= 19) {
     return {
       태양의은총: 36,
       태양의축복: 18,
       태양의가호: 6,
     };
-  } else if (refineNumber >= 22) {
+  } else if (refineNumber >= 20 && refineNumber <= 25) {
     return {
       태양의은총: 48,
       태양의축복: 24,
