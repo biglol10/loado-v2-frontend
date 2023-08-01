4db92fd19bad1b9f65b770463c841e05610180fd
biglol <biglol@nate.com>
2023-08-01 16:24:16 +0900

    itemprice page with mobile size and price modal done

--- src/pages/ItemPrice.tsx
+++ src/pages/ItemPrice.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable no-await-in-loop */
 import React, { useState, useEffect, useMemo } from 'react';
-import BaseService from '@services/BaseService';
-import axios from 'axios';
 import styled from 'styled-components';
-import LOSTARK_API from '@consts/api';
 import useDeviceType from '@hooks/DeviceTypeHook';
 import { MainTable } from '@components/atoms/table/index';
-import { ITableData } from '@components/atoms/table/Types';
 import { IsMobile } from '@consts/interfaces';
-import { useQuery, QueryKey } from '@tanstack/react-query';
+import { useQueries } from '@tanstack/react-query';
 import { getMarketPriceByCategoryCode } from '@services/ItemPriceService';
-import { toast } from 'react-toastify';
-import { StyledDiv } from '@consts/appStyled';
+import _ from 'lodash';
 
 const TopTab = styled.div<IsMobile>`
   .tab-list {
@@ -19,6 +14,7 @@ const TopTab = styled.div<IsMobile>`
     width: ${(props) => (props.isMobile ? '100%' : '500px')};
     list-style: none;
     margin: 0;
+    margin-bottom: 30px;
     padding: 0;
     background: #343540;
   }
@@ -42,93 +38,150 @@ const TopTab = styled.div<IsMobile>`
   }
 `;
 
-const TableWrapper = styled.div`
-  display: inline;
+const TableWrapper = styled.div<IsMobile>`
+  display: grid;
+  grid-template-columns: ${(props) => (props.isMobile ? '1fr' : '1fr 1fr')};
+  gap: 50px;
 `;
 
-export type ActiveTabType = 'all' | 'book' | 'material' | 'mylist';
+export type ActiveTabType = 'all' | 'book' | 'material' | 'mylist' | 'esderAndGem';
+
+export interface ItemData {
+  recordId: string;
+  itemName: string;
+  itemId: string;
+  categoryCode: number;
+  date: string;
+  minCurrentMinPrice: number;
+  maxCurrentMinPrice: number;
+  avgCurrentMinPrice: number;
+}
+
+const dataOrderInRefineType = [
+  '명예의 파편 주머니(소)',
+  '명예의 파편 주머니(중)',
+  '명예의 파편 주머니(대)',
+  '명예의 돌파석',
+  '위대한 명예의 돌파석',
+  '경이로운 명예의 돌파석',
+  '찬란한 명예의 돌파석',
+  '상급 오레하 융화 재료',
+  '최상급 오레하 융화 재료',
+  '파괴강석',
+  '정제된 파괴강석',
+  '수호강석',
+  '정제된 수호강석',
+];
+
+const dataOrderInAdditionalRefineType = [
+  '태양의 은총',
+  '태양의 축복',
+  '태양의 가호',
+  '야금술 : 단조 심화',
+  '재봉술 : 수선 심화',
+  '야금술 : 단조 숙련',
+  '재봉술 : 수선 숙련',
+  '야금술 : 단조 특화',
+  '재봉술 : 수선 특화',
+];
 
 const ItemPricePage = () => {
   const [activeTab, setActiveTab] = useState<ActiveTabType>('all');
-  const [refinement, setRefinement] = useState([]);
-  const [refinementAdditional, setRefinementAdditional] = useState([]);
-  const [etc, setEtc] = useState([]);
-  const [asder, setAsder] = useState([]);
-  const [engravings, setEngravings] = useState([]);
-  const [jewlery, setJewlery] = useState([]);
+  const [refinement, setRefinement] = useState<ItemData[]>([]);
+  const [refinementAdditional, setRefinementAdditional] = useState<ItemData[]>([]);
+  const [etc, setEtc] = useState<ItemData[]>([]);
+  const [asder, setAsder] = useState<ItemData[]>([]);
+  const [engravings, setEngravings] = useState<ItemData[]>([]);
+  const [characterEngravings, setCharacterEngravings] = useState<ItemData[]>([]);
+  const [jewlery, setJewlery] = useState<ItemData[]>([]);
   const deviceType = useDeviceType();
-  const initalCategoryCodes = ['50010', '50020', '51000', '51100', '210000'];
-  const [categoryCode, setCategoryCode] = useState<string>('50020');
+  const initalCategoryCodes = ['44410', '44420', '50010', '50020', '51000', '51100', '210000'];
+  // const [categoryCode, setCategoryCode] = useState<string>('50020');
 
-  const queryKey: QueryKey = ['marketPrice', categoryCode];
+  const returnDataByStringArray = (data: ItemData[], arr: string[]) => {
+    const orderedData = _.orderBy(data, (item) => {
+      const index = arr.indexOf(item.itemName);
 
-  const itemsQuery = useQuery(queryKey, () => getMarketPriceByCategoryCode(categoryCode), {
-    onSuccess: (data: any) => {
-      switch (categoryCode) {
-        case '50010':
-          setRefinement(data);
-          break;
-        case '50020':
-          setRefinementAdditional(data);
-          break;
-        case '51000':
-          setEtc(data);
-          break;
-        case '51100':
-          setAsder(data);
-          break;
-        case '44420':
-          setEngravings(data);
-          break;
-        case '210000':
-          setJewlery(data);
-          break;
-        default:
-          break;
-      }
-    },
-    staleTime: 1000 * 60 * 5,
-    refetchOnWindowFocus: false,
-    enabled: true,
+      return index !== -1 ? index : Infinity;
+    });
+
+    return orderedData;
+  };
+
+  const queries = useQueries({
+    queries: initalCategoryCodes.map((code) => {
+      return {
+        queryKey: ['marketPrice2', code],
+        queryFn: () => getMarketPriceByCategoryCode(code),
+        onSuccess: (data: ItemData[]) => {
+          switch (code) {
+            case '44410':
+              setEngravings(data.sort((a, b) => b.minCurrentMinPrice - a.minCurrentMinPrice));
+              break;
+            case '44420':
+              setCharacterEngravings(
+                data.sort((a, b) => b.minCurrentMinPrice - a.minCurrentMinPrice),
+              );
+              break;
+            case '50010':
+              setRefinement(returnDataByStringArray(data, dataOrderInRefineType));
+              break;
+            case '50020':
+              setRefinementAdditional(
+                returnDataByStringArray(data, dataOrderInAdditionalRefineType),
+              );
+              break;
+            case '51000':
+              setEtc(data);
+              break;
+            case '51100':
+              setAsder(data);
+              break;
+            case '210000':
+              setJewlery(data);
+              break;
+            default:
+              break;
+          }
+        },
+        onError: (error: any) => {
+          console.log('Error:', error);
+        },
+        staleTime: 1000 * 60 * 5,
+        enabled: true,
+      };
+    }),
   });
 
-  if (itemsQuery.status === 'error') {
-    toast.error(<StyledDiv color="black">데이터를 가져오지 못했습니다</StyledDiv>, {
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 2000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: false,
-    });
-    return null;
-  }
+  useEffect(() => {
+    queries.forEach((query) => query.refetch());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const columns: string[] = useMemo(
-    () => ['각인서명', '전일 평균 거래가', '최근 거래가', '최저가', '시세조회', '관심등록'],
-    [],
-  );
+  const columns = useMemo(() => {
+    if (deviceType === 'mobile') {
+      return ['아이템명', '최소가격', '시세조회'];
+    } else {
+      return ['아이템명', '최소가격', '평균가격', '최대가격', '시세조회'];
+    }
+  }, [deviceType]);
 
-  const MainTableWrapper = ({ children }: { children: React.ReactElement }) => {
-    return (
-      <div>
-        {React.Children.map(children, (el) => {
-          return React.cloneElement(el, { activeTab });
-        })}
-      </div>
-    );
-  };
+  const columnsForBook = useMemo(() => {
+    if (deviceType === 'mobile') {
+      return ['각인서명', '최소가격', '시세조회'];
+    } else {
+      return ['각인서명', '최소가격', '평균가격', '최대가격', '시세조회'];
+    }
+  }, [deviceType]);
 
   return (
     <>
-      {/* <div>asdf</div> */}
       <TopTab isMobile={deviceType === 'mobile'}>
         <ul className="tab-list">
           <li
             className={`tab-item ${activeTab === 'all' ? 'active' : ''}`}
             onClick={() => {
               setActiveTab('all');
-              setCategoryCode('50020');
             }}
           >
             <span className="tab-link">전체</span>
@@ -137,7 +190,6 @@ const ItemPricePage = () => {
             className={`tab-item ${activeTab === 'book' ? 'active' : ''}`}
             onClick={() => {
               setActiveTab('book');
-              setCategoryCode('44420');
             }}
           >
             <span className="tab-link">각인서</span>
@@ -146,45 +198,89 @@ const ItemPricePage = () => {
             className={`tab-item ${activeTab === 'material' ? 'active' : ''}`}
             onClick={() => {
               setActiveTab('material');
-              setCategoryCode('50010');
             }}
           >
             <span className="tab-link">재련재료</span>
           </li>
-          <li
+          {/* <li
             className={`tab-item ${activeTab === 'mylist' ? 'active' : ''}`}
             onClick={() => {
               setActiveTab('mylist');
             }}
           >
             <span className="tab-link">내 관심</span>
+          </li> */}
+          <li
+            className={`tab-item ${activeTab === 'esderAndGem' ? 'active' : ''}`}
+            onClick={() => {
+              setActiveTab('esderAndGem');
+            }}
+          >
+            <span className="tab-link">에스더/보석</span>
           </li>
         </ul>
       </TopTab>
-      <TableWrapper>
-        {(activeTab === 'all' || activeTab === 'material') && (
-          <>
+      {activeTab === 'all' && (
+        <TableWrapper isMobile={deviceType === 'mobile'}>
+          <div>
             <MainTable headerTitle="재련 재료" data={refinement ?? []} columns={columns} />
             <MainTable
               headerTitle="재련 추가 재료"
               data={refinementAdditional ?? []}
               columns={columns}
             />
-            <MainTable headerTitle="기타 재료" data={etc ?? [] ?? []} columns={columns} />
-          </>
-        )}
-        {(activeTab === 'all' || activeTab === 'book') && (
-          <>
-            <MainTable headerTitle="각인서" data={engravings ?? []} columns={columns} />
-          </>
-        )}
-        {activeTab === 'all' && (
-          <>
-            <MainTable headerTitle="에스더의 기운" data={asder ?? []} columns={columns} />
-            <MainTable headerTitle="보석" data={jewlery ?? []} columns={columns} />
-          </>
-        )}
-      </TableWrapper>
+            <MainTable headerTitle="엘릭서" data={etc ?? [] ?? []} columns={columns} />
+            <MainTable
+              headerTitle="에스더의 기운 / 보석"
+              data={[...asder, ...jewlery] ?? []}
+              columns={columns}
+            />
+          </div>
+          <div>
+            <MainTable headerTitle="각인서" data={engravings ?? []} columns={columnsForBook} />
+            <MainTable
+              headerTitle="직업각인서"
+              data={characterEngravings ?? []}
+              columns={columnsForBook}
+            />
+          </div>
+        </TableWrapper>
+      )}
+      {activeTab !== 'all' && (
+        <TableWrapper>
+          {activeTab === 'material' && (
+            <>
+              <MainTable headerTitle="재련 재료" data={refinement ?? []} columns={columns} />
+              <MainTable
+                headerTitle="재련 추가 재료"
+                data={refinementAdditional ?? []}
+                columns={columns}
+              />
+              <MainTable headerTitle="엘릭서" data={etc ?? [] ?? []} columns={columns} />
+            </>
+          )}
+          {activeTab === 'book' && (
+            <>
+              <MainTable headerTitle="각인서" data={engravings ?? []} columns={columnsForBook} />
+              <MainTable
+                headerTitle="직업각인서"
+                data={characterEngravings ?? []}
+                columns={columnsForBook}
+              />
+            </>
+          )}
+          {activeTab === 'esderAndGem' && (
+            <>
+              <MainTable
+                headerTitle="에스더의 기운 / 보석"
+                data={[...asder, ...jewlery] ?? []}
+                columns={columns}
+              />
+              <MainTable headerTitle="엘릭서" data={etc ?? [] ?? []} columns={columns} />
+            </>
+          )}
+        </TableWrapper>
+      )}
     </>
   );
 };
